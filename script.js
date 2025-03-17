cf43884511692ab06b184bc75d219cc4ba1081bc
abhishekdixit07 <87979374+abhishekdixit07@users.noreply.github.com>
2025-03-17 20:18:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-let cost=0;
+var cost=0;
 arr.sort();
-arr.map((item,i)=>{
-	if(i==arr.length){
-		return;
-	}
-	cost=cost+arr[i]+arr[i+1];
-	arr[i+1]=arr[i]+arr[i+1];
+arr.reduce((t=0,i)=>{
+	cost=cost+i;
+}
+		  return cost;);
 	
-	return cost;
-})
   
 }
 
