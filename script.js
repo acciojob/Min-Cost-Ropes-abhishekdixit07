7f962eb0e0a5cab6068a1d3f3e507fa062b1f135
abhishekdixit07 <87979374+abhishekdixit07@users.noreply.github.com>
2025-03-17 20:34:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-var cost=0;
-arr.sort();
-arr.reduce((t=0,i)=>{
-	cost=cost+i;
-}
-		  return cost;);
-	
+ // Create a min-heap from the array elements
+  let heap = new MinHeap();
+  for(let i = 0; i < arr.length; i++) {
+    heap.insert(arr[i]);
+  }
+  
+  let cost = 0;
+  
+  while (heap.size() > 1) {
+    // Extract the two smallest elements
+    let first = heap.extractMin();
+    let second = heap.extractMin();
+    
+    // Add the extracted elements to the total cost
+    cost += first + second;
+    
+    // Push the sum of the extracted elements back to the heap
+    heap.insert(first + second);
+  }
+  
+  return cost;
   
 }
 
