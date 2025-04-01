bd3eabb95a258c39ef625499089edf6977542ea0
abhishekdixit07 <87979374+abhishekdixit07@users.noreply.github.com>
2025-04-01 19:11:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-function mincost(arr)
-{ 
-let cost=0;	
-let n=arr.length;
-arr.sort((a,b)=>a-b);
-while (arr.length > 1) {
-        // Remove the first two ropes
+function mincost(arr) {
+    let cost = 0;
+    arr.sort((a, b) => a - b);
+    while (arr.length > 1) {
         let a = arr.shift();
         let b = arr.shift();
-        // Add their lengths to cost
         cost += a + b;
-        // Insert a + b back into arr in its proper position
-        // You can use a function like Array.prototype.splice() for this
-        // ...
+        let i = arr.findIndex(x => x >= a + b);
+        arr.splice(i === -1 ? arr.length : i, 0, a + b);
     }
     return cost;
 }
