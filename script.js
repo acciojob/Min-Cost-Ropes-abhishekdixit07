96ba653155c29840bf33702142214e309c71b811
abhishekdixit07 <87979374+abhishekdixit07@users.noreply.github.com>
2025-04-01 19:07:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ function mincost(arr)
 { 
 let cost=0;	
 let n=arr.length;
-arr.sort;
-arr.map((rope,i)=>{
-	if((i+1)>n){
-		break;
-	}
-	cost=arr[i]+arr[i+1];
-	arr[i+1]=arr[i]+arr[i+1];
-}
-		return cost;
+arr.sort((a,b)=>a-b);
+while (arr.length > 1) {
+        // Remove the first two ropes
+        let a = arr.shift();
+        let b = arr.shift();
+        // Add their lengths to cost
+        cost += a + b;
+        // Insert a + b back into arr in its proper position
+        // You can use a function like Array.prototype.splice() for this
+        // ...
+    }
+    return cost;
 }
 
 module.exports=mincost;
